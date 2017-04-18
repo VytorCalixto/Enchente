90d493168ba7c49286f3e1a9e5ec3578c05f1e1f
Vytor Calixto <vytorcalixto@gmail.com>
2017-04-18 16:49:15 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]) {
+    printf("Ler o tabuleiro (em matriz?)\n");
+    printf("Enquanto pilha não vazia:\n");
+    printf("\tPega os adjacentes do grupo (retorna os pesos dos filhos = montar o grafo)\n");
+    printf("\tEscolhe filho de maior peso\n");
+    printf("\tEmpilha os outros filhos\n");
+    printf("\tSE tamanho do caminho atual + filhos do nodo atual >= menor caminho: VOLTA!\n");
+    printf("\tSE soma do peso total = tamanho do tabuleiro: VOLTA! Retorna o caminho encontrado\n");
+    return 0;
+}
+
+// pilha global
+// {
+//      identificador: "passo"
+//      valor original
+//      posição
+// }
+
+// {
+//      identificador: 4
+//      valor original: 2
+//      posição: 0,0
+// },
+// {
+//      identificador: 5
+//      valor original: 4
+//      posição: 0,0 - 0,1 - 1,0 - 2,0
+// }
+//
