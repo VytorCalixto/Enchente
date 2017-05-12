bb1bdfc10a619320d91ff10becbbe2ea23773889
Vytor Calixto <vytorcalixto@gmail.com>
2017-05-12 10:28:39 -0300

    Add lista do grupo inicial

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <time.h>
 #include "libs/tabuleiro.h"
+#include "libs/no.h"
+#include "libs/lista.h"
 #include "libs/grafo.h"
 
 int main() {
@@ -13,8 +15,17 @@ int main() {
         return -1;
     }
     Grafo g = criaGrafo();
+    // Transforma o tabuleiro para um grafo
     tabuleiroParaGrafo(t, g);
+    // Lista com o grupo inicial
+    Lista grupo = constroiLista();
+    insereLista(t->celulas[0]->vert, grupo); // A célula na posição 0 do array é a célula no canto superior esquerdo
+
+    // Desaloca lista do grupo
+    destroiLista(grupo, NULL);
+    // Desaloca tabuleiro
     destroiTblr(t);
+    // Desaloca grafo
     destroiGrafo(g);
     return 0;
 }
