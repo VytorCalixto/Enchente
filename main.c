2def96dfec236d3df6f8de7cc6dbdda581447ef3
Vytor Calixto <vytorcalixto@gmail.com>
2017-05-11 23:33:09 -0300

    Modifica main para ler o tabuleiro e montar o grafo

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "libs/tabuleiro.h"
+#include "libs/grafo.h"
 
 int main() {
     Tblr t = criaTblr();
@@ -11,37 +12,9 @@ int main() {
         puts("Erro na leitura do tabuleiro");
         return -1;
     }
-    imprimeTblr(t);
-
-    // // declara o grafo ponderado e seu primeiro nodo
-    // grafo = criaGrafo(t);
-    // if(grafo == NULL)) {
-    //     puts("Erro na criacao do grafo");
-    //     return -1;
-    // }
-    // // faz o jogador jogar o jogo a partir do tabuleiro dado
-    // // o retorno do jogador sera um vetor contendo os numeros referentes a menor jogada encontrada por ele
-    // int *jogadas;
-    // jogadas = Joga(grafo, t);
-
+    Grafo g = criaGrafo();
+    tabuleiroParaGrafo(t, g);
+    destroiTblr(t);
+    destroiGrafo(g);
     return 0;
 }
-
-// pilha global
-// {
-//      identificador: "passo"
-//      valor original
-//      posição
-// }
-
-// {
-//      identificador: 4
-//      valor original: 2
-//      posição: 0,0
-// },
-// {
-//      identificador: 5
-//      valor original: 4
-//      posição: 0,0 - 0,1 - 1,0 - 2,0
-// }
-//
